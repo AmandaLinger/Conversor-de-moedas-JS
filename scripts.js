2cc9abbe714b6c8b4b0db2ae8f242d94265a0d58
Amanda Marques <amandamarqueslinger@gmail.com>
2025-11-26 16:22:03 -0300

    get selected coin

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
+const form = document.querySelector("form")
+
 const amount = document.getElementById("amount");
+const coin = document.getElementById("coin")
 
 //manipulando o input amount para receber apenas numeros
 amount.addEventListener("input", () => {
 
-    const hasCharactersRegex = /\D+/g
+    const hasCharactersRegex = /\D+/g;
 
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+
+//capturando o evento de submite do formulário
+form.onsubmit = (event) => {
+    event.preventDefault();
+    console.log(coin.value)
+}
