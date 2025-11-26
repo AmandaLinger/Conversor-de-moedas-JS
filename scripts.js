eb5fcc5d9499a4434319eadd479a18aae933e6e9
Amanda Marques <amandamarqueslinger@gmail.com>
2025-11-26 16:11:41 -0300

    validating the input

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const amount = document.getElementById("amount");
+
+//manipulando o input amount para receber apenas numeros
+amount.addEventListener("input", () => {
+
+    const hasCharactersRegex = /\D+/g
+
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
